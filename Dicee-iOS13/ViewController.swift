1db00c4a1631099bbb7ab55e1c02f7eade18172d
Michael526-dev <michael.chia@bunkerchain.io>
2025-05-24 12:29:34 +0800

    Initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //click on control and hode to here from the image to initialize the image
+    //if want to change the name(variable name) in the code, you can right click on the name and then refactor -> change name so that all the name in the whole system will be change
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBOutlet weak var rollButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceTwo");
+        diceImageView2.image = UIImage(named: "DiceSix");
+
+        
     }
 
 
